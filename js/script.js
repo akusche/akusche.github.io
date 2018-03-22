d250e737d121cfa8fff863e314c2b7077c5ae419
Alexander Kusche <akusche@icloud.com>
2018-03-22 10:43:29 +0100

    fix of commit

--- js/script.js
+++ js/script.js
@@ -17,7 +17,6 @@ function loaded()
 	localizeHTMLTag("preise");
 	localizeHTMLTag("anfahrt");
 	localizeHTMLTag("kontakt");
-
 }
 
 
@@ -43,4 +42,33 @@ function getParameterValue(parameter)
 		return "";
 	else
 		return results[1];
+}
+
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
 }
