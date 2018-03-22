cf6d87e86e28b4385cee0f72e123dad467f0724c
Alexander Kusche <akusche@icloud.com>
2018-03-22 11:49:42 +0100

    neue image gallery eingebaut

--- js/script.js
+++ js/script.js
@@ -17,6 +17,9 @@ function loaded()
 	localizeHTMLTag("preise");
 	localizeHTMLTag("anfahrt");
 	localizeHTMLTag("kontakt");
+	
+	var slideIndex = 1;
+	showSlides(slideIndex);
 }
 
 
@@ -45,12 +48,10 @@ function getParameterValue(parameter)
 }
 
 
-// Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
@@ -63,12 +64,14 @@ function showSlides(n) {
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+      slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+      dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
   captionText.innerHTML = dots[slideIndex-1].alt;
 }
+
+    
