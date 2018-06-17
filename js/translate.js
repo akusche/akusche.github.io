41aa47005a40067a4c9edda3119083dcfde57a00
akusche <akusche@icloud.com>
2018-06-17 13:39:08 -0500

    Update translate.js
    Spanisch Sprache & Sprache wird automatisch erkannt und gespeichert

--- js/translate.js
+++ js/translate.js
@@ -1,68 +1,116 @@
 var t=new Array();
-t['en']=new Array();
-t['de']=new Array();
-	t['en']['headline_1']='About us';
-	t['de']['headline_1']='Über uns';
-	t['en']['headline_2']='Rooms';
-	t['de']['headline_2']='Räume';
-	t['en']['headline_3']='Facilities';
-	t['de']['headline_3']='Ausstattung';
-	t['en']['headline_4']='Contact';
-	t['de']['headline_4']='Kontakt';
-	
-	t['en']['headline_1_text']='The Living Hope Guest House offers a relaxing and peaceful accomodation characterized by a unique fusion of Nepali hospitality and German living standard. Sunita and Alexander do their best to create a place of rest for every guest in the otherwise quite noisy Kathmandu. The near Ringroad makes day trips by bus or taxi to famous sights in Kathmandu very easy. Supermarkets can be reached by walk within a few minutes. A lot of good cafés and restaurants can be found nearby in Jawalakhel and Jhamsikhel.';
-	t['de']['headline_1_text']='Das Living Hope Guest House bietet eine wohltuend friedliche Unterkunft, ausgezeichnet durch eine einzigartige Fusion nepalesischer Gastfreundschaft mit deutschem Wohnstandard. Sunita und Alexander tun ihr Bestes, um jedem Gast in dem sonst so lärmigen Kathmandu einen Ort der Ruhe zu schaffen. Die naheliegende Hauptstraße ermöglicht einfache Ausflüge per Bus oder Taxi zu den beliebtesten Sehenswürdigkeiten in Kathmandu. Supermärkte sind in wenigen Minuten zu Fuß erreichbar. Auch zu den mit Cafés und Restaurants übersäten Stadtteilen Jawalakhel und Jhamsikhel ist es nicht weit.';	
-	t['en']['headline_2_text']='All together there are 9 guestrooms with space for up to 21 guests. Three triple rooms, five standard double rooms and one double room with attached bathroom. The other bathrooms, kitchens and living rooms are shared by all guests.';
-	t['de']['headline_2_text']='Insgesamt verfügen wir über 9 Gästezimmer mit Platz für bis zu 21 Gäste.  Drei 3er-Zimmer, fünf normale Doppelzimmer und ein Doppelzimmer mit eigenem Bad. Die anderen Badezimmer, Küchen und Aufenthaltsräume werden von allen Gästen gemeinsam genutzt.';
-	t['en']['headline_2_text_2']='Double room with Attached Bathroom: 2500 NRs/Room/Night (max. 2 Persons)';
-	t['de']['headline_2_text_2']='Doppelzimmer mit eigenem Bad: 2500 NRs/Zimmer/Nacht (max. 2 Personen)';
-	t['en']['headline_2_text_3']='Double rooms: 2000 NRs/Room/Night (max. 2 Persons)';
-	t['de']['headline_2_text_3']='Doppelzimmer: 2000 NRs/Zimmer/Nacht (max. 2 Personen)';	
-	t['en']['headline_2_text_4']='Triple rooms: 1000 NRs/Person/Night';
-	t['de']['headline_2_text_4']='3er-Zimmer: 1000 NRs/Person/Nacht';	
-	t['en']['headline_2_text_5']='For long-term stays cheaper offers can be made.';
-	t['de']['headline_2_text_5']='Für längere Aufenthalte kann über günstigere Preise gesprochen werden.';	
-	
-	t['en']['headline_3_text']='Further extras:';
-	t['de']['headline_3_text']='Weitere Extras:';	
-	t['en']['headline_3_text_2']='free WiFi';
-	t['de']['headline_3_text_2']='kostenloses WLAN';
-	t['en']['headline_3_text_3']='fully equipped kitchens';
-	t['de']['headline_3_text_3']='voll ausgestattete Küchen zur freien Verfügung';
-	t['en']['headline_3_text_4']='gym room';
-	t['de']['headline_3_text_4']='Fitnessraum';	
-	t['en']['headline_3_text_5']='roofterrace with space to hang out';
-	t['de']['headline_3_text_5']='Dachterrasse mit Sitzmöglichkeiten';
-	t['en']['headline_3_text_6']='hair dryer';
-	t['de']['headline_3_text_6']='Haartrockner';
-	t['en']['headline_3_text_7']='laundry service*';
-	t['de']['headline_3_text_7']='Wäscheservice*';
-	t['en']['headline_3_text_8']='barbecue*';
-	t['de']['headline_3_text_8']='Kohlegrill*';
-	t['en']['headline_3_text_9']='*for a fee';
-	t['de']['headline_3_text_9']='*gegen Gebühr';
+var langs = ["en", "de", "es"];
+t['en']=new Array();	
+t['de']=new Array();	
+t['es']=new Array();
+		
+t['en']['headline_1']='About us';	
+t['de']['headline_1']='Über uns';	
+t['es']['headline_1']='Sobre nosotros';	
+t['en']['headline_2']='Rooms';	
+t['de']['headline_2']='Räume';	
+t['es']['headline_2']='Nuestras habitaciones';		
+t['en']['headline_3']='Facilities';	
+t['de']['headline_3']='Ausstattung';	
+t['es']['headline_3']='Instalaciones y servicios';			
+t['en']['headline_4']='Contact';	
+t['de']['headline_4']='Kontakt';	
+t['es']['headline_4']='Contacto';		
+t['en']['headline_1_text']='The Living Hope Guest House offers a relaxing and peaceful accomodation characterized by a unique fusion of Nepali hospitality and German living standard. Sunita and Alexander do their best to create a place of rest for every guest in the otherwise quite noisy Kathmandu. The near Ringroad makes day trips by bus or taxi to famous sights in Kathmandu very easy. Supermarkets can be reached by walk within a few minutes. A lot of good cafés and restaurants can be found nearby in Jawalakhel and Jhamsikhel.';	
+t['de']['headline_1_text']='Das Living Hope Guest House bietet eine wohltuend friedliche Unterkunft, ausgezeichnet durch eine einzigartige Fusion nepalesischer Gastfreundschaft mit deutschem Wohnstandard. Sunita und Alexander tun ihr Bestes, um jedem Gast in dem sonst so lärmigen Kathmandu einen Ort der Ruhe zu schaffen. Die naheliegende Hauptstraße ermöglicht einfache Ausflüge per Bus oder Taxi zu den beliebtesten Sehenswürdigkeiten in Kathmandu. Supermärkte sind in wenigen Minuten zu Fuß erreichbar. Auch zu den mit Cafés und Restaurants übersäten Stadtteilen Jawalakhel und Jhamsikhel ist es nicht weit.';		
+t['es']['headline_1_text']='Living Hope Guest House ofrece un alojamiento relajante y tranquilo caracterizado por una fusión única de la hospitalidad Nepalí y el estándar de vida Alemán. Sunita y Alexander hacen todo lo posible para crear un lugar de descanso para que cada uno de nuestros huéspedes puedan olvidarse de la ruidosa Katmandú. En las cercanías, Ringroad hace que los viajes de un día a lugares de interés turístico a Katmandú sean muy sencillos a través de autobús o taxi. En los alrededores se puede llegar a los supermercados en unos pocos minutos a pie, al igual que a buenos cafés y restaurantes ubicados en Jawalakhel y Jhamsikhel.';			
+t['en']['headline_2_text']='All together there are 9 guestrooms with space for up to 21 guests. Three triple rooms, five standard double rooms and one double room with attached bathroom. The other bathrooms, kitchens and living rooms are shared by all guests.';	
+t['de']['headline_2_text']='Insgesamt verfügen wir über 9 Gästezimmer mit Platz für bis zu 21 Gäste.  Drei 3er-Zimmer, fünf normale Doppelzimmer und ein Doppelzimmer mit eigenem Bad. Die anderen Badezimmer, Küchen und Aufenthaltsräume werden von allen Gästen gemeinsam genutzt.';	
+t['es']['headline_2_text']='Contamos con un total de 9 habitaciones con espacio para 21 huéspedes. Tres habitaciones triples, cinco habitaciones estándar dobles y una habitación doble con baño privado. El resto de baños, cocinas y salas de estar son estancias compartidas.';		
+t['en']['headline_2_text_2']='Double room with Attached Bathroom: 2500 NRs/Room/Night (max. 2 Persons)';
+t['de']['headline_2_text_2']='Doppelzimmer mit eigenem Bad: 2500 NRs/Zimmer/Nacht (max. 2 Personen)';	
+t['es']['headline_2_text_2']='Habitación doble con baño privado: 2500 NRs / Habitación / Noche (máximo 2 personas)';		
+t['en']['headline_2_text_3']='Double rooms: 2000 NRs/Room/Night (max. 2 Persons)';	
+t['de']['headline_2_text_3']='Doppelzimmer: 2000 NRs/Zimmer/Nacht (max. 2 Personen)';		
+t['es']['headline_2_text_3']='Habitaciones dobles con baño compartido: 2000 NRs / Habitación / Noche (máximo 2 personas)';			
+t['en']['headline_2_text_4']='Triple rooms: 1000 NRs/Person/Night';	
+t['de']['headline_2_text_4']='3er-Zimmer: 1000 NRs/Person/Nacht';		
+t['es']['headline_2_text_4']='Habitaciones triples: 1000 NRs por persona / Noche';		
+t['en']['headline_2_text_5']='For long-term stays cheaper offers can be made.';	
+t['de']['headline_2_text_5']='Für längere Aufenthalte kann über günstigere Preise gesprochen werden.';		
+t['es']['headline_2_text_5']='Para estancias a largo plazo se pueden realizar ofertas más económicas.';				
+t['en']['headline_3_text']='Further extras:';	
+t['de']['headline_3_text']='Weitere Extras:';	
+t['es']['headline_3_text']='Otros extras';			
+t['en']['headline_3_text_2']='free WiFi';	
+t['de']['headline_3_text_2']='kostenloses WLAN';	
+t['es']['headline_3_text_2']='Wi-Fi gratis';		
+t['en']['headline_3_text_3']='fully equipped kitchens';	
+t['de']['headline_3_text_3']='voll ausgestattete Küchen zur freien Verfügung';	
+t['es']['headline_3_text_3']='Cocinas completamente equipadas';			
+t['en']['headline_3_text_4']='gym room';	
+t['de']['headline_3_text_4']='Fitnessraum';		
+t['es']['headline_3_text_4']='Gimnasio';			
+t['en']['headline_3_text_5']='roofterrace with space to hang out';	
+t['de']['headline_3_text_5']='Dachterrasse mit Sitzmöglichkeiten';	
+t['es']['headline_3_text_5']='Terraza con espacio de descanso';		
+t['en']['headline_3_text_6']='hair dryer';	
+t['de']['headline_3_text_6']='Haartrockner';	
+t['es']['headline_3_text_6']='Secador de pelo';		
+t['en']['headline_3_text_7']='laundry service*';	
+t['de']['headline_3_text_7']='Wäscheservice*';	
+t['es']['headline_3_text_7']='Servicio de lavandería*';	
+t['en']['headline_3_text_8']='barbecue*';	
+t['de']['headline_3_text_8']='Kohlegrill*';	
+t['es']['headline_3_text_8']='Barbacoa*';		
+t['en']['headline_3_text_9']='*for a fee';	
+t['de']['headline_3_text_9']='*gegen Gebühr';	
+t['es']['headline_3_text_9']='*bajo coste';	
+t['en']['headline_4_text']='Phone:';	
+t['de']['headline_4_text']='Telefon:';	
+t['es']['headline_4_text']='Teléfono';			
+t['en']['headline_4_text_2']='design & realisation by A. Kusche © 2018';	
+t['de']['headline_4_text_2']='Design & Umsetzung durch A. Kusche © 2018';	
+t['es']['headline_4_text_2']='Diseño e implementación por A. Kusche © 2018';		
+t['en']['headline_4_text_3']='photos by E. Baumann © 2018';	
+t['de']['headline_4_text_3']='Fotos durch E. Baumann © 2018';	
+t['es']['headline_4_text_3']='Fotos por E. Baumann © 2018';
 
-	t['en']['headline_4_text']='Phone:';
-	t['de']['headline_4_text']='Telefon:';
-	
-	t['en']['headline_4_text_2']='design & realisation by A. Kusche © 2018';
-	t['de']['headline_4_text_2']='Design & Umsetzung durch A. Kusche © 2018';
-	
-	t['en']['headline_4_text_3']='photos by E. Baumann © 2018';
-	t['de']['headline_4_text_3']='Fotos durch E. Baumann © 2018';
-
-$(document).ready(function() {
-	//initilize the lang
-	var lang = 'en';
-    $('.trn').each(function(i){
-        $(this).text(t[lang][ $(this).attr('key') ]);
-    });
-    
-    // onclick behavior
-    $('.lang').click( function() {
-        var lang = $(this).attr('id');
-        $('.trn').each(function(i){
-          $(this).text(t[lang][ $(this).attr('key') ]);
+$(document).ready(function() {	
+//initilize the default lang
+var lang = 'en';	
+// check for language cookie	
+if(langs.indexOf(getCookie("lang")) > -1) { 
+lang = getCookie("lang"); }	
+// else check for browser language	
+else if (navigator.language.indexOf("de") > -1) { lang = 'de'; } 	
+else if (navigator.language.indexOf("es") > -1) { lang = 'es'; }
+$('.trn').each(function(i){        
+	$(this).text(t[lang][ $(this).attr('key') ]);
+});   
+// onclick behavior   
+$('.lang').click( function() {        
+	var lang = $(this).attr('id');     
+	setCookie("lang", lang, "30");
+        alert(getCookie("lang"));
+	$('.trn').each(function(i){        
+		$(this).text(t[lang][ $(this).attr('key') ]);
         });
-    } );
+} );        
+function setCookie(cname, cvalue, exdays) {    	
+	var d = new Date();   	
+	d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    	var expires = "expires="+ d.toUTCString();
+    	document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+	
+function getCookie(cname) {
+   var name = cname + "=";
+   var decodedCookie = decodeURIComponent(document.cookie);
+   var ca = decodedCookie.split(';');
+   for(var i = 0; i <ca.length; i++) {
+       var c = ca[i];
+       while (c.charAt(0) == ' ') {
+       	c = c.substring(1);
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";	
+}
 });
